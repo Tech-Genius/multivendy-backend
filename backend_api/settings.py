89300023a571c07729bef1e3e63f1c68b7951440
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-12-02 04:49:30 +0100

    fix db

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -110,12 +110,12 @@ DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
