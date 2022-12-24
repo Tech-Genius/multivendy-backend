76a2f6df2eca275d2cd97531585ae554a0c6d58d
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-12-24 14:27:41 +0100

    trying to host on render

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-# python manage.py loaddata data.json
+python manage.py shell 
+from django.contrib.contenttypes.models import ContentType
+ContentType.objects.all().delete()
+exit()
+python manage.py loaddata data.json
