96ab9431c789ab4d3de18e1e6e1bf1ae58fb08fa
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-12-24 16:07:58 +0100

    trying to host on render

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py sqlflush | python manage.py dbshell
 # python manage.py remove_stale_contenttypes
 # python manage.py shell 
 # from django.contrib.contenttypes.models import ContentType
