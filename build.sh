bbb295cdb615234806dc31f2598c8e1a4da47474
Tech-Genius <adebimpehabeeb10@gmail.com>
2023-03-10 05:25:11 +0100

    feat: authentication

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
 python manage.py migrate
 python manage.py sqlflush | python manage.py dbshell
 # python manage.py remove_stale_contenttypes
